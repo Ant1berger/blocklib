0cd4d56c807c627e205b0b75c13f5d16a7c8c93b
Antoine Berger <graphisme@antoineberger.com>
2024-11-18 19:04:02 +0100

    Replace hovers to not loose them, but I will put them in modifiers later..

--- custom-blocks/build/grouping-link/index.asset.php
+++ custom-blocks/build/grouping-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd87159f08d3a06e5b2e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0a9206fdaa46e7a28b70');
--- custom-blocks/build/knob/index.asset.php
+++ custom-blocks/build/knob/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c55096c3db1b8934c33c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5d9d51a01ef53a68ef66');
