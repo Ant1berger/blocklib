c859f4c5d41b14ddc8e161e59d7f74601e7b936a
Antoine Berger <graphisme@antoineberger.com>
2024-11-18 19:00:58 +0100

    Remove hovers to allow customized ones.

--- custom-blocks/build/knob/index.asset.php
+++ custom-blocks/build/knob/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '15d6cec001d02f1ad6c3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c55096c3db1b8934c33c');
