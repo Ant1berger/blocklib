afce85af05992ad6886c645b18f3f7f271d3c450
Antoine Berger <graphisme@antoineberger.com>
2024-12-05 18:30:49 +0100

    Finally managed to find a simpler, cleaner and steadier solution for sharing main styles between the Editor and the Front-end and keep them dynamic.

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_dequeue_style( 'classic-theme-styles' );
 }, 20 );
 
-/*  DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
+/* DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
 function wps_deregister_styles() {
     wp_dequeue_style( 'global-styles' );
 }
@@ -129,21 +129,18 @@ function addMainCssJs() {
         )
     );
 
-    // We take our main files...
-    $main_css_path = get_template_directory() . '/main-auto-generated-dont-edit-me.css';
+    // We take our main styles and scripts...
+    $main_css_content = get_custom_block_common_styles();
     $main_js_path = get_template_directory() . '/main.js';
 
     // ... And we inject their content inside our registered <style> and <script> shells.
-    if (file_exists($main_css_path)) {
-        $main_css_content = file_get_contents($main_css_path);
-        wp_add_inline_style('blb-main', $main_css_content);
-    }
+    wp_add_inline_style('blb-main', $main_css_content);
     if (file_exists($main_js_path)) {
         $main_js_content = file_get_contents($main_js_path);
         wp_add_inline_script('blb-main-inline', $main_js_content);
     }
 
-    // And let's finally enqueue everybody.
+    // And let's finally enqueue all this.
     wp_enqueue_style('blb-main');
     wp_enqueue_script('blb-main-inline');
 }
@@ -185,7 +182,7 @@ add_action( 'init', 'theme_register_blocks' );
 function custom_allowed_block_types( $allowed_block_types, $block_editor_context ) {
     $allowed_block_types = array(
         'core/paragraph', // To keep the ability to insert blocks, copy/paste etc.
-        'core/block', // To allow create Compositions.
+        'core/block', // To allow the creation of Patterns.
         'custom-blocks/text',
         'custom-blocks/title',
         'custom-blocks/stage',
@@ -201,33 +198,6 @@ function custom_allowed_block_types( $allowed_block_types, $block_editor_context
 add_filter( 'allowed_block_types_all', 'custom_allowed_block_types', 10, 2 );
 
 //****************************
-// to insert theme variables into the main CSS.
-//****************************
-function handleThemeOptionsForCSSVarsPlaceholders( $optionId ) {
-    $css_vars_array = array();
-    foreach(get_option( $optionId ) as $key => $value) {
-        if ( isset($value) && !empty($value) ) {
-            $css_vars_array[] .= '    --' . $key . ': ' . $value . ';';
-        }
-    }
-    return implode("\n", $css_vars_array);
-}
-
-//****************************
-// To dynamically insert values in the main.css file.
-// It's not possible to use get_template_directory_uri() or other php stuff directly in the main.css file because it's not a php file.
-// If we try to change the css file extension to php, it will be loaded properly by wp_enqueue_style, but the styles won't be applied.
-// So we have to read the main.css file, replace the placeholders with the dynamic values we need,
-// and write it back in an auto-generated file that we can finally load in Gutenberg with wp_enqueue_style.
-//****************************
-$cssString = file_get_contents(get_template_directory_uri() . '/main.css');
-$cssString = str_replace('[get_template_directory_uri_placeholder]', get_template_directory_uri(), $cssString);
-// We also have to make placeholders for theme customizer options.
-$cssString = str_replace('/*colors_placholder*/', handleThemeOptionsForCSSVarsPlaceholders( 'theme_colors' ), $cssString);
-$cssString = str_replace('/*fonts_placholder*/', handleThemeOptionsForCSSVarsPlaceholders( 'theme_fonts' ), $cssString);
-file_put_contents(get_template_directory() . '/main-auto-generated-dont-edit-me.css', $cssString);
-
-//****************************
 // Add styles to Admin.
 //****************************
 function theme_admin_styles() {
@@ -236,20 +206,18 @@ function theme_admin_styles() {
 add_action('admin_enqueue_scripts', 'theme_admin_styles');
 
 //****************************
-// Add styles to Gutenberg editor.
+// Add styles and scripts to Gutenberg editor.
 //****************************
+// For the Gutenberg interface.
 function add_scripts_to_gutenberg() {
     wp_enqueue_style( 'additional-gutenberg-styles', get_theme_file_uri('/additional-gutenberg-styles.css'), array(), '1.0');
     wp_enqueue_script( 'additional-gutenberg-scripts', get_theme_file_uri('/additional-gutenberg-scripts.js'), array(), '1.0');
 }
 add_action('enqueue_block_editor_assets', 'add_scripts_to_gutenberg');
-
-function theme_setup() {
-    // enables the support for editor styles inside the editor iframe.
-    add_theme_support( 'editor-styles' );
-    add_editor_style( get_theme_file_uri('/main-auto-generated-dont-edit-me.css') );
-}
-add_action( 'after_setup_theme', 'theme_setup' );
+// For inside the editing area iframe to get a true WYSIWYG.
+add_action('enqueue_block_editor_assets', function() {
+    wp_add_inline_style('wp-edit-blocks', get_custom_block_common_styles());
+});
 
 //****************************
 // Add theme customizer options.
@@ -474,5 +442,295 @@ function fix_wp_get_attachment_image_svg($image, $attachment_id, $size, $icon) {
         }
     }
     return $image;
+}
+
+//****************************
+// to insert theme variables into the main CSS.
+//****************************
+function writeCssVariablesFromThemeOptions( $optionId ) {
+    $css_vars_array = array();
+    foreach(get_option( $optionId ) as $key => $value) {
+        if ( isset($value) && !empty($value) ) {
+            $css_vars_array[] .= '    --' . $key . ': ' . $value . ';';
+        }
+    }
+    return implode("\n", $css_vars_array);
+}
+
+//****************************
+// Global and dynamic website styles for both the front-end <head> and the Gutenberg editor.
+// I'd have prefered to put them in a separated PHP file but I couldn't do it
+// nor from a file include (or equivalent) because servers might forbid them in functions.php for security reasons,
+// nor from a global variable,
+// nor from a filter. None of those three worked. Let's try something else some time.
+//****************************
+function get_custom_block_common_styles() {
+    return '
+/**********************************/
+/* Global styles for every pages */
+/* Order matters: keep variables first */
+/*************************************/
+
+/**************/
+/* Variables */
+/************/
+
+/* As a reminder for media queries:
+    $sm: 480px;
+    $md: 768px;
+    $lg: 992px;
+    $xl: 1280px;
+*/
+:root {
+
+' . writeCssVariablesFromThemeOptions( 'theme_colors' ) . '
+
+' . writeCssVariablesFromThemeOptions( 'theme_fonts' ) . '
+
+    /* Some recurring lengths */
+    --stagePadding: 1rem;
+    --headerPadding: 1.2rem;
+    --headerHeight: 8rem;
+    &:has(.-pageScrolled) {
+        --headerHeight: 6rem;
+    }
+
+    @media(min-width: 992px) {
+        --stagePadding: 3rem;
+        --headerPadding: 1.8rem;
+        --headerHeight: 12rem;
+        &:has(.-pageScrolled) {
+            --headerHeight: 8rem;
+        }
+    }
+}
+
+/**********/
+/* Fonts */
+/********/
+
+/* math */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: italic;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-italic-math.woff2) format("woff2");
+    unicode-range: U+0302-0303, U+0305, U+0307-0308, U+0330, U+0391-03A1, U+03A3-03A9, U+03B1-03C9, U+03D1, U+03D5-03D6, U+03F0-03F1, U+03F4-03F5, U+2034-2037, U+2057, U+20D0-20DC, U+20E1, U+20E5-20EF, U+2102, U+210A-210E, U+2110-2112, U+2115, U+2119-211D, U+2124, U+2128, U+212C-212D, U+212F-2131, U+2133-2138, U+213C-2140, U+2145-2149, U+2190, U+2192, U+2194-21AE, U+21B0-21E5, U+21F1-21F2, U+21F4-2211, U+2213-2214, U+2216-22FF, U+2308-230B, U+2310, U+2319, U+231C-2321, U+2336-237A, U+237C, U+2395, U+239B-23B6, U+23D0, U+23DC-23E1, U+2474-2475, U+25AF, U+25B3, U+25B7, U+25BD, U+25C1, U+25CA, U+25CC, U+25FB, U+266D-266F, U+27C0-27FF, U+2900-2AFF, U+2B0E-2B11, U+2B30-2B4C, U+2BFE, U+FF5B, U+FF5D, U+1D400-1D7FF, U+1EE00-1EEFF;
+}
+/* symbols */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: italic;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-italic-symbol.woff2) format("woff2");
+    unicode-range: U+0001-000C, U+000E-001F, U+007F-009F, U+20DD-20E0, U+20E2-20E4, U+2150-218F, U+2190, U+2192, U+2194-2199, U+21AF, U+21E6-21F0, U+21F3, U+2218-2219, U+2299, U+22C4-22C6, U+2300-243F, U+2440-244A, U+2460-24FF, U+25A0-27BF, U+2800-28FF, U+2921-2922, U+2981, U+29BF, U+29EB, U+2B00-2BFF, U+4DC0-4DFF, U+FFF9-FFFB, U+10140-1018E, U+10190-1019C, U+101A0, U+101D0-101FD, U+102E0-102FB, U+10E60-10E7E, U+1D2C0-1D2D3, U+1D2E0-1D37F, U+1F000-1F0FF, U+1F100-1F1AD, U+1F1E6-1F1FF, U+1F30D-1F30F, U+1F315, U+1F31C, U+1F31E, U+1F320-1F32C, U+1F336, U+1F378, U+1F37D, U+1F382, U+1F393-1F39F, U+1F3A7-1F3A8, U+1F3AC-1F3AF, U+1F3C2, U+1F3C4-1F3C6, U+1F3CA-1F3CE, U+1F3D4-1F3E0, U+1F3ED, U+1F3F1-1F3F3, U+1F3F5-1F3F7, U+1F408, U+1F415, U+1F41F, U+1F426, U+1F43F, U+1F441-1F442, U+1F444, U+1F446-1F449, U+1F44C-1F44E, U+1F453, U+1F46A, U+1F47D, U+1F4A3, U+1F4B0, U+1F4B3, U+1F4B9, U+1F4BB, U+1F4BF, U+1F4C8-1F4CB, U+1F4D6, U+1F4DA, U+1F4DF, U+1F4E3-1F4E6, U+1F4EA-1F4ED, U+1F4F7, U+1F4F9-1F4FB, U+1F4FD-1F4FE, U+1F503, U+1F507-1F50B, U+1F50D, U+1F512-1F513, U+1F53E-1F54A, U+1F54F-1F5FA, U+1F610, U+1F650-1F67F, U+1F687, U+1F68D, U+1F691, U+1F694, U+1F698, U+1F6AD, U+1F6B2, U+1F6B9-1F6BA, U+1F6BC, U+1F6C6-1F6CF, U+1F6D3-1F6D7, U+1F6E0-1F6EA, U+1F6F0-1F6F3, U+1F6F7-1F6FC, U+1F700-1F7FF, U+1F800-1F80B, U+1F810-1F847, U+1F850-1F859, U+1F860-1F887, U+1F890-1F8AD, U+1F8B0-1F8B1, U+1F900-1F90B, U+1F93B, U+1F946, U+1F984, U+1F996, U+1F9E9, U+1FA00-1FA6F, U+1FA70-1FA7C, U+1FA80-1FA88, U+1FA90-1FABD, U+1FABF-1FAC5, U+1FACE-1FADB, U+1FAE0-1FAE8, U+1FAF0-1FAF8, U+1FB00-1FBFF;
+}
+/* latin-ext */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: italic;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-italic-latinext.woff2) format("woff2");
+    unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: italic;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-italic-latin.woff2) format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* math */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: normal;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-normal-math.woff2) format("woff2");
+    unicode-range: U+0302-0303, U+0305, U+0307-0308, U+0330, U+0391-03A1, U+03A3-03A9, U+03B1-03C9, U+03D1, U+03D5-03D6, U+03F0-03F1, U+03F4-03F5, U+2034-2037, U+2057, U+20D0-20DC, U+20E1, U+20E5-20EF, U+2102, U+210A-210E, U+2110-2112, U+2115, U+2119-211D, U+2124, U+2128, U+212C-212D, U+212F-2131, U+2133-2138, U+213C-2140, U+2145-2149, U+2190, U+2192, U+2194-21AE, U+21B0-21E5, U+21F1-21F2, U+21F4-2211, U+2213-2214, U+2216-22FF, U+2308-230B, U+2310, U+2319, U+231C-2321, U+2336-237A, U+237C, U+2395, U+239B-23B6, U+23D0, U+23DC-23E1, U+2474-2475, U+25AF, U+25B3, U+25B7, U+25BD, U+25C1, U+25CA, U+25CC, U+25FB, U+266D-266F, U+27C0-27FF, U+2900-2AFF, U+2B0E-2B11, U+2B30-2B4C, U+2BFE, U+FF5B, U+FF5D, U+1D400-1D7FF, U+1EE00-1EEFF;
+}
+/* symbols */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: normal;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-normal-symbol.woff2) format("woff2");
+    unicode-range: U+0001-000C, U+000E-001F, U+007F-009F, U+20DD-20E0, U+20E2-20E4, U+2150-218F, U+2190, U+2192, U+2194-2199, U+21AF, U+21E6-21F0, U+21F3, U+2218-2219, U+2299, U+22C4-22C6, U+2300-243F, U+2440-244A, U+2460-24FF, U+25A0-27BF, U+2800-28FF, U+2921-2922, U+2981, U+29BF, U+29EB, U+2B00-2BFF, U+4DC0-4DFF, U+FFF9-FFFB, U+10140-1018E, U+10190-1019C, U+101A0, U+101D0-101FD, U+102E0-102FB, U+10E60-10E7E, U+1D2C0-1D2D3, U+1D2E0-1D37F, U+1F000-1F0FF, U+1F100-1F1AD, U+1F1E6-1F1FF, U+1F30D-1F30F, U+1F315, U+1F31C, U+1F31E, U+1F320-1F32C, U+1F336, U+1F378, U+1F37D, U+1F382, U+1F393-1F39F, U+1F3A7-1F3A8, U+1F3AC-1F3AF, U+1F3C2, U+1F3C4-1F3C6, U+1F3CA-1F3CE, U+1F3D4-1F3E0, U+1F3ED, U+1F3F1-1F3F3, U+1F3F5-1F3F7, U+1F408, U+1F415, U+1F41F, U+1F426, U+1F43F, U+1F441-1F442, U+1F444, U+1F446-1F449, U+1F44C-1F44E, U+1F453, U+1F46A, U+1F47D, U+1F4A3, U+1F4B0, U+1F4B3, U+1F4B9, U+1F4BB, U+1F4BF, U+1F4C8-1F4CB, U+1F4D6, U+1F4DA, U+1F4DF, U+1F4E3-1F4E6, U+1F4EA-1F4ED, U+1F4F7, U+1F4F9-1F4FB, U+1F4FD-1F4FE, U+1F503, U+1F507-1F50B, U+1F50D, U+1F512-1F513, U+1F53E-1F54A, U+1F54F-1F5FA, U+1F610, U+1F650-1F67F, U+1F687, U+1F68D, U+1F691, U+1F694, U+1F698, U+1F6AD, U+1F6B2, U+1F6B9-1F6BA, U+1F6BC, U+1F6C6-1F6CF, U+1F6D3-1F6D7, U+1F6E0-1F6EA, U+1F6F0-1F6F3, U+1F6F7-1F6FC, U+1F700-1F7FF, U+1F800-1F80B, U+1F810-1F847, U+1F850-1F859, U+1F860-1F887, U+1F890-1F8AD, U+1F8B0-1F8B1, U+1F900-1F90B, U+1F93B, U+1F946, U+1F984, U+1F996, U+1F9E9, U+1FA00-1FA6F, U+1FA70-1FA7C, U+1FA80-1FA88, U+1FA90-1FABD, U+1FABF-1FAC5, U+1FACE-1FADB, U+1FAE0-1FAE8, U+1FAF0-1FAF8, U+1FB00-1FBFF;
+}
+/* latin-ext */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: normal;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-normal-latinext.woff2) format("woff2");
+    unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+    font-family: "Bodoni Moda";
+    font-style: normal;
+    font-weight: 400 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/bodonimoda-normal-latin.woff2) format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* cyrillic */
+@font-face {
+    font-family: "Jost";
+    font-style: italic;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-italic-cyrillic.woff2) format("woff2");
+    unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+/* latin-ext */
+@font-face {
+    font-family: "Jost";
+    font-style: italic;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-italic-latinext.woff2) format("woff2");
+    unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+    font-family: "Jost";
+    font-style: italic;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-italic-latin.woff2) format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* cyrillic */
+@font-face {
+    font-family: "Jost";
+    font-style: normal;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-normal-cyrillic.woff2) format("woff2");
+    unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+/* latin-ext */
+@font-face {
+    font-family: "Jost";
+    font-style: normal;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-normal-latinext.woff2) format("woff2");
+    unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+    font-family: "Jost";
+    font-style: normal;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url(' . get_template_directory_uri() . '/assets/fonts/jost-normal-latin.woff2) format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+/***********/
+/* Global */
+/*********/
+
+/* carefull with those two : test contrast for accessibility. Should be optional. */
+/* * {
+    scrollbar-color: var(--brand_1) var(--brand_2);
+}
+
+::selection {
+    color: var(--brand_2);
+    background-color: var(--brand_1);
+} */
+
+html {
+    font-size: 62.5%;
+    -webkit-text-size-adjust: none; /* for iOS Safari */
+    text-size-adjust: none; /* for other mobile browsers */
+}
+
+@media(prefers-reduced-motion: no-preference) {
+
+    html {
+        scroll-behavior: smooth;
+    }
+}
+
+body {
+    margin: 0;
+    font-family: var(--font_1);
+    font-size: 1.6rem;
+    color: var(--grey_start);
+    min-inline-size: 32rem;
+    min-block-size: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    &:has(.menuOpen) {
+        overflow: hidden;
+
+        @media(min-width: 1240px) {
+            overflow: visible;
+        }
+    }
+}
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    border: none;
+}
+
+label,
+button,
+select,
+summary,
+[type=radio],
+[type=submit],
+[type=checkbox] {
+    cursor: pointer;
+}
+
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+display: block;
+}
+
+.noScript {
+    background-color: var(--grey_end);
+    padding: 1rem;
+    color: var(--grey_start);
+    display: block;
+    position: relative;
+    z-index: 1000000;
+}
+
+/* To hide page title on editing area. */
+.editor-visual-editor__post-title-wrapper {
+    display: none;
+}
+
+/* To see transparent header in composition editor. */
+.block-editor-iframe__body:not(.post-type-page) {
+    background-color: #eeeeee;
+}
+';
 }
 ?>
